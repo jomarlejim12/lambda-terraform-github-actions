3b09fca2af41e7ab1c7584e12204bd668e08e7b3
jomarlejim12 <jomarlejim80@gmail.com>
2024-03-07 16:18:17 +0800

    COMMIT NOW

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,9 +3,9 @@ variable "project_name" {
 }
 
 variable "region" {
-  default = "eu-west-3"
+  default = "us-east-1"
 }
 
-variable "profile" {
-  default = "default"
-}
+# variable "profile" {
+#   default = "default"
+# }
